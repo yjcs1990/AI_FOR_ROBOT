f0138206d1164e5cab83fe6e4cfdaece980e1911
yjcs1990 <yjcs1990@163.com>
2016-07-01 08:29:23 +0800

    The uniform Distribution of probability

---
+++ Uniform_Distribution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- encoding:utf-8 -*-
+"""
+@version: ??
+@author: Jay
+@license: BSD Licence 
+@file: Uniform_Distribution.py
+@time: 2016/6/30 23:32
+"""
+p = []
+n = 5
+
+for i in range(n):
+	p.append(1.0/n)
+
+print p
